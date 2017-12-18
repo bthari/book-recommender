a38b2cc331f6bbb4398f4bbb10cfd292636cd76a
Glory Finesse Valery <glory.fvr@gmail.com>
2017-12-18 22:29:40 +0700

    Create models & insert data to database

--- retrieve/models.py
+++ retrieve/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+	book_id = models.IntegerField()
+	title = models.CharField(max_length=128)
+	description = models.TextField(default='-')
